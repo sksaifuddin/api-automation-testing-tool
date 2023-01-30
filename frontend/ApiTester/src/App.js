b635ada1f8c647ec3b9bcc6dd197edea19391ca0
Raj Patel <rj540530@dal.ca>
2023-01-30 18:38:37 -0400

    Created frontend project

---
+++ frontend/ApiTester/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import DbTest from './components/DbTest';
+
+function App() {
+  return (
+    <div className="App">
+      <DbTest/>
+    </div>
+  );
+}
+
+export default App;
