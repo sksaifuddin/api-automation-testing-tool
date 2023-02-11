419884600a1b003e3dbb0510a8028f553284cea7
Saif <shaiksaifuddin95@gmail.com>
2023-02-10 21:43:41 -0400

    Basic-setup:created a project from spring initializer

---
+++ backend/api-db-tester/src/main/java/com/project/apidbtester/ApiDbTesterApplication.java
@@ -0,0 +1,13 @@
+package com.project.apidbtester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDbTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDbTesterApplication.class, args);
+	}
+
+}
---
+++ backend/api-db-tester/src/test/java/com/project/apidbtester/ApiDbTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.apidbtester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDbTesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
