b46279b45672d48e176b7f31b7cccb6ee10dd075
Shubham Mishra <sh277446@dal.ca>
2023-02-17 13:24:09 -0400

    fetch all table names in the client db

--- backend/api-db-tester/src/main/java/com/project/apidbtester/service/ClientDBDetailsService.java
+++ backend/api-db-tester/src/main/java/com/project/apidbtester/service/ClientDBDetailsService.java
@@ -4,6 +4,10 @@ import com.project.apidbtester.error.ClientDBConnectionException;
 import com.project.apidbtester.model.ClientDBDetails;
 import com.project.apidbtester.model.CustomApiResponseBody;
 
+import java.util.List;
+import java.util.Map;
+
 public interface ClientDBDetailsService {
     public CustomApiResponseBody testClientDBConnection(ClientDBDetails clientDBDetails) throws ClientDBConnectionException;
+    public Map<String, List<String>> fetchClientDBSchema(ClientDBDetails clientDBDetails) throws ClientDBConnectionException;
 }
--- backend/api-db-tester/src/main/java/com/project/apidbtester/service/ClientDBDetailsServiceImpl.java
+++ backend/api-db-tester/src/main/java/com/project/apidbtester/service/ClientDBDetailsServiceImpl.java
@@ -4,12 +4,18 @@ import com.project.apidbtester.error.ClientDBConnectionException;
 import com.project.apidbtester.model.ClientDBDetails;
 import com.project.apidbtester.model.CustomApiResponseBody;
 import com.project.apidbtester.repository.ClientDBDetailsRepository;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 
-import java.sql.DriverManager;
+import java.net.ConnectException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ClientDBDetailsServiceImpl implements ClientDBDetailsService {
@@ -23,6 +29,32 @@ public class ClientDBDetailsServiceImpl implements ClientDBDetailsService {
             DriverManager.getConnection(clientDBDetails.getDatabaseUrl(), clientDBDetails.getUserName(), clientDBDetails.getPassword());
             CustomApiResponseBody successResponse = new CustomApiResponseBody(HttpStatus.valueOf(200), "DB Connection Successful", 200);
             return successResponse;
+        } catch (Exception e) {
+            throw new ClientDBConnectionException("Database connection Failed, please check the details again");
+        }
+    }
+
+    @Override
+    public Map<String, List<String>> fetchClientDBSchema(ClientDBDetails clientDBDetails) throws ClientDBConnectionException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(clientDBDetails.getDatabaseUrl(), clientDBDetails.getUserName(), clientDBDetails.getPassword());
+
+            Statement statement = connection.createStatement();
+//            DatabaseMetaData databaseMetaData = connection.getMetaData();
+//            ResultSet resultSet = databaseMetaData.getTables(null, null, null, new String[]{"SYSTEM TABLE"});
+
+            ResultSet resultSet = statement.executeQuery("show tables");
+                    List<String> tables = new ArrayList<>();
+            while (resultSet.next()) {
+                String tableName = resultSet.getString(1);
+                if (tableName.contains("_seq")==false) {
+                    tables.add(tableName);
+                }
+            }
+            Map<String, List<String>> map = new HashMap<>();
+            map.put("1", tables);
+            return map;
         } catch (Exception e) {
             throw new ClientDBConnectionException("Database connection Failed, please check the details again");
         }
