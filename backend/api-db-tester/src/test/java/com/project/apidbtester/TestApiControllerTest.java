a57e40f1ed072fd3cd6a6cba559079fd6c9a6f90
Shubham Mishra <sh277446@dal.ca>
2023-03-28 11:24:42 -0300

    Add Test cases for TestApiController

---
+++ backend/api-db-tester/src/test/java/com/project/apidbtester/TestApiControllerTest.java
@@ -0,0 +1,102 @@
+package com.project.apidbtester;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import com.project.apidbtester.testapis.controllers.TestApiController;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.project.apidbtester.testapis.entities.TestCaseDetails;
+import com.project.apidbtester.testapis.dtos.TestInput;
+import com.project.apidbtester.testapis.dtos.TestResponse;
+import com.project.apidbtester.testapis.services.DeleteApiService;
+import com.project.apidbtester.testapis.services.GetApiService;
+import com.project.apidbtester.testapis.services.PostApiService;
+import com.project.apidbtester.testapis.services.PutApiService;
+
+@ExtendWith(MockitoExtension.class)
+public class TestApiControllerTest {
+
+	@Mock
+	private PostApiService postApiService;
+	@Mock
+	private PutApiService putApiService;
+	@Mock
+	private DeleteApiService deleteApiService;
+	@Mock
+	private GetApiService getApiService;
+	@InjectMocks
+	private TestApiController controller;
+
+	@Test
+	public void testFetchTestResultWithPostRequest() {
+		TestInput testInput = new TestInput();
+		TestCaseDetails testCaseDetails = new TestCaseDetails();
+		testCaseDetails.setType("POST");
+		testInput.setTestCaseDetails(testCaseDetails);
+		TestResponse testResponse = new TestResponse();
+		testResponse.setHttpStatusCode(HttpStatus.OK.value());
+		when(postApiService.fetchTestResult(testInput)).thenReturn(testResponse);
+		ResponseEntity<TestResponse> response = controller.fetchTestResult(testInput);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		verify(postApiService, times(1)).fetchTestResult(testInput);
+	}
+
+	@Test
+	public void testFetchTestResultWithPutRequest() {
+		TestInput testInput = new TestInput();
+		TestCaseDetails testCaseDetails = new TestCaseDetails();
+		testCaseDetails.setType("PUT");
+		testInput.setTestCaseDetails(testCaseDetails);
+		TestResponse testResponse = new TestResponse();
+		testResponse.setHttpStatusCode(HttpStatus.OK.value());
+		when(putApiService.fetchTestResult(testInput)).thenReturn(testResponse);
+		ResponseEntity<TestResponse> response = controller.fetchTestResult(testInput);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		verify(putApiService, times(1)).fetchTestResult(testInput);
+	}
+
+	@Test
+	public void testFetchTestResultWithGetRequest() {
+		TestInput testInput = new TestInput();
+		TestCaseDetails testCaseDetails = new TestCaseDetails();
+		testCaseDetails.setType("GET");
+		testInput.setTestCaseDetails(testCaseDetails);
+		TestResponse testResponse = new TestResponse();
+		testResponse.setHttpStatusCode(HttpStatus.OK.value());
+		when(getApiService.fetchTestResult(testInput)).thenReturn(testResponse);
+		ResponseEntity<TestResponse> response = controller.fetchTestResult(testInput);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		verify(getApiService, times(1)).fetchTestResult(testInput);
+	}
+
+	@Test
+	public void testFetchTestResultWithDeleteRequest() {
+		TestInput testInput = new TestInput();
+		TestCaseDetails testCaseDetails = new TestCaseDetails();
+		testCaseDetails.setType("DELETE");
+		testInput.setTestCaseDetails(testCaseDetails);
+		TestResponse testResponse = new TestResponse();
+		testResponse.setHttpStatusCode(HttpStatus.OK.value());
+		when(deleteApiService.fetchTestResult(testInput)).thenReturn(testResponse);
+		ResponseEntity<TestResponse> response = controller.fetchTestResult(testInput);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		verify(deleteApiService, times(1)).fetchTestResult(testInput);
+	}
+
+	@Test
+	public void testFetchTestResultWithInvalidRequestType() throws TestApiController.InvalidRequestTypeException {
+		TestInput testInput = new TestInput();
+		TestCaseDetails testCaseDetails = new TestCaseDetails();
+		testCaseDetails.setType("INVALID_REQUEST_TYPE");
+		testInput.setTestCaseDetails(testCaseDetails);
+		assertThrows(TestApiController.InvalidRequestTypeException.class, () -> controller.fetchTestResult(testInput));
+	}
+
+}
