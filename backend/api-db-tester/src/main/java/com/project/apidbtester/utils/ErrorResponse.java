964053b33c047306afae7929283cb3f3662300e5
Shubham Mishra <sh277446@dal.ca>
2023-04-08 13:41:05 -0300

    Add utils package documentation

--- backend/api-db-tester/src/main/java/com/project/apidbtester/utils/ErrorResponse.java
+++ backend/api-db-tester/src/main/java/com/project/apidbtester/utils/ErrorResponse.java
@@ -3,6 +3,9 @@ package com.project.apidbtester.utils;
 import lombok.Builder;
 import lombok.Data;
 
+/**
+ * ErrorResponse is used to return an object containing error message
+ */
 @Data
 @Builder
 public class ErrorResponse {
--- backend/api-db-tester/src/main/java/com/project/apidbtester/utils/Query.java
+++ backend/api-db-tester/src/main/java/com/project/apidbtester/utils/Query.java
@@ -5,12 +5,26 @@ import com.project.apidbtester.testapis.entities.TestColumnValue;
 
 import java.util.List;
 
+/**
+ * Query class is used to generate SQL queries from information provided to it
+ */
 public class Query {
 
+    /**
+     * fetch all rows of a table
+     * @param tableName name of the table
+     * @return generated query
+     */
     public static String generateSelectQuery(String tableName) {
         return "select * from " + tableName + ";";
     }
 
+    /**
+     * fetch specific rows with specific columns of a table
+     * @param testColumnValues details of columns to be fetched
+     * @param testCaseDetails details of test performed needed for the primary key column name and value
+     * @return generated query
+     */
     public static String generateSelectQueryWithWhereClause(List<TestColumnValue> testColumnValues, TestCaseDetails testCaseDetails) {
         StringBuilder query = new StringBuilder("select ");
 
@@ -30,6 +44,11 @@ public class Query {
         return query.toString();
     }
 
+    /**
+     * fetch the count of rows specific to a primary key value in a table
+     * @param testCaseDetails details of test performed needed for the primary key column name and value
+     * @return generated query
+     */
     public static String generateCountQueryWithWhereClause(TestCaseDetails testCaseDetails) {
         StringBuilder query = new StringBuilder("select count(*)");
 
